9d8bc2dac547598e78bcea41e087a7a8319f52db
HMBSbige <admin@bige0.com>
2018-09-20 20:03:04 +0800

    demo

---
+++ UPnP/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UPnP
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			var client = new UPnP(IPAddress.Parse(@"192.168.199.212"), 4489, 1551, ProtocolType.Udp, @"UPnP测试");
+			client.Add();
+			Thread.Sleep(10000);
+			client.Remove();
+		}
+	}
+}
---
+++ UPnP/UPnP.cs
@@ -0,0 +1,49 @@
+﻿using NATUPNPLib;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UPnP
+{
+	class UPnP
+	{
+		public readonly IPAddress InternalIP;
+		public readonly int ExternalPort;
+		public readonly int InternalPort;
+		public readonly ProtocolType Type;
+		public readonly string Description;
+
+		private readonly UPnPNAT UPnPnat;
+
+		private IStaticPortMappingCollection Mappings => UPnPnat.StaticPortMappingCollection;
+		private string Typestr => Type.ToString().ToUpper();
+
+		public UPnP(IPAddress internalIp, int externalPort, int internalPort, ProtocolType type, string description)
+		{
+			UPnPnat = new UPnPNAT();
+
+			InternalIP = internalIp;
+			ExternalPort = externalPort;
+			InternalPort = internalPort;
+			Type = type;
+			Description = description;
+		}
+
+		public void Add()
+		{
+			if (Mappings == null)
+			{
+				Debug.WriteLine(@"无法连接到路由器，或者路由器关闭\不支持 UPnP。");
+				return;
+			}
+
+			//添加之前的ipv4变量（内网IP），内部端口，和外部端口
+			Mappings.Add(ExternalPort, Typestr, InternalPort, InternalIP.ToString(), true, Description);
+		}
+
+		public void Remove()
+		{
+			Mappings.Remove(ExternalPort, Typestr);
+		}
+	}
+}
